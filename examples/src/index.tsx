be9714a3bc466ad280aaab748e091c6c89691f47
Jorg Nieberg <jnieberg@gmail.com>
2022-09-14 09:33:16 +0200

    Moved netlify config up

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -2,11 +2,14 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import React from "react";
+import { Router } from "wouter";
 
 const root = createRoot(document.getElementById("root")! as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
