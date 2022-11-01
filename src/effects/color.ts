6c0db2d3f24b117fc073bcae55252d43b752eaf5
Jorg Nieberg <jnieberg@gmail.com>
2022-11-01 14:34:46 +0100

    Color prop optimized for alpha channeled images

--- src/effects/color.ts
+++ src/effects/color.ts
@@ -5,21 +5,17 @@ import { LayerProps } from "../types";
 export const effectColor = (ctx: CanvasRenderingContext2D, props: LayerProps) => {
   const isShape = props.circle || props.curve || props.line || props.rect || props.text;
   if (props.color && !isShape) {
-    // TO DO - find a way to make this generally work for both SVG and other image types
-    if (props.src?.search(/\.svg$/) === -1) {
-      newCanvasHelper(ctx, (ctxColor) => {
+    newCanvasHelper(ctx, (ctxColor) => {
+      ctxColor.drawImage(ctx.canvas, 0, 0);
+      ctxColor.globalCompositeOperation = "source-in";
+      ctxColor.fillStyle = !props.color || typeof props.color === "boolean" ? DEFAULT.color : props.color;
+      ctxColor.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      // TO DO - find a way to make this generally work for both SVG and other image types
+      if (props.src?.search(/\.svg$/) === -1) {
         ctxColor.filter = "grayscale(100%)";
+        ctxColor.globalCompositeOperation = "hard-light";
         ctxColor.drawImage(ctx.canvas, 0, 0);
-        ctxColor.filter = DEFAULT.filter;
-        ctxColor.globalCompositeOperation = "multiply";
-        ctxColor.fillStyle = !props.color || typeof props.color === "boolean" ? DEFAULT.color : props.color;
-        ctxColor.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      });
-    } else {
-      ctx.globalCompositeOperation = "source-in";
-      ctx.fillStyle = typeof props.color === "boolean" ? DEFAULT.color : props.color;
-      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      ctx.globalCompositeOperation = "source-over";
-    }
+      }
+    });
   }
 };
