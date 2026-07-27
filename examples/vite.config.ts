fabcbbb29daba9f75f92371d235fb86b3a3e6b2c
Jorg Nieberg <j.nieberg@belastingdienst.nl>
2026-07-27 12:19:43 +0200

    config fixes

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -5,11 +5,6 @@ import { fileURLToPath } from "url";
 // https://vite.dev
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "react-three-texture": fileURLToPath(new URL("../src/index.ts", import.meta.url)),
-    },
-  },
   server: {
     port: 3000, // Matches the old Create React App default port
     open: true, // Automatically opens the app in your browser on startup
